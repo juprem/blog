4a793b97bce6404bb889c0bc698b3dd845cf0280
ABDELNOUR Olivier (EXT-CAR-FR) <olivier.abdelnour@cartier.com>
2025-06-06 10:56:53 +0200

    feat: add of ci github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createRouter, createMemoryHistory } from '@tanstack/react-router';
 import './index.css';
 import 'dayjs/locale/fr';
 
@@ -10,8 +10,15 @@ import dayjs from 'dayjs';
 
 dayjs.locale('fr');
 
+const memoryHistory = createMemoryHistory({
+    initialEntries: ['/'], // Pass your initial url
+});
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+    routeTree,
+    basepath: '/blog/',
+    history: memoryHistory,
+});
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
