9eb466822af41e3a5d09677820fd4e7338482989
oabdelnour <olivier.abdelnour@gmail.com>
2025-06-06 10:56:53 +0200

    feat: add of ci github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter, createMemoryHistory } from '@tanstack/react-router';
+import './index.css';
+import 'dayjs/locale/fr';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+import dayjs from 'dayjs';
+
+dayjs.locale('fr');
+
+const memoryHistory = createMemoryHistory({
+    initialEntries: ['/'], // Pass your initial url
+});
+// Create a new router instance
+const router = createRouter({
+    routeTree,
+    basepath: '/blog/',
+    history: memoryHistory,
+});
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>,
+    );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,77 @@
+import { createFileRoute, FileRoutesByPath } from '@tanstack/react-router';
+import { ArticleCard } from '../components/ArticleCard/ArticleCard.tsx';
+import dayjs, { Dayjs } from 'dayjs';
+import RandomImage from '../assets/random_image.jpg';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button.tsx';
+
+export const Route = createFileRoute('/')({
+    component: Index,
+});
+
+const articles: {
+    img: string;
+    title: string;
+    date: Dayjs;
+    readingTime: number;
+    link: keyof FileRoutesByPath;
+}[] = [
+    {
+        date: dayjs(),
+        img: RandomImage,
+        readingTime: 7,
+        title: 'Premier Article',
+        link: '/first-article',
+    },
+    {
+        date: dayjs(),
+        img: RandomImage,
+        readingTime: 7,
+        title: 'Premier Article2',
+        link: '/first-article',
+    },
+    {
+        date: dayjs(),
+        img: RandomImage,
+        readingTime: 7,
+        title: 'Premier Article3',
+        link: '/first-article',
+    },
+    {
+        date: dayjs(),
+        img: RandomImage,
+        readingTime: 7,
+        title: 'Premier Article4',
+        link: '/first-article',
+    },
+    {
+        date: dayjs(),
+        img: RandomImage,
+        readingTime: 7,
+        title: 'Premier Article5',
+        link: '/first-article',
+    },
+];
+
+function Index() {
+    const [counter, setCounter] = useState(0);
+    return (
+        <div className="p-2 grid max-lg:grid-cols-1 grid-cols-2 2xl:grid-cols-3 gap-4 justify-items-center">
+            <Button onClick={() => setCounter(counter + 1)}>T + {counter}</Button>
+            {articles.map((article) => {
+                const { title, date, img, link, readingTime } = article;
+
+                return (
+                    <ArticleCard
+                        date={date}
+                        img={img}
+                        readingTime={readingTime}
+                        title={title}
+                        link={link}
+                        key={`${title}-${date}`}
+                    />
+                );
+            })}
+        </div>
+    );
+}
